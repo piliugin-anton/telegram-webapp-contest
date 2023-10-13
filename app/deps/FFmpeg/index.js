4910e6947bb892c9c529c84d47d282c7454bda6b
Anton Piliugin <anton.piliugin@icloud.com>
2023-10-13 15:34:32 +0500

    FFmpeg exception handling

--- app/deps/FFmpeg/index.js
+++ app/deps/FFmpeg/index.js
@@ -9,48 +9,52 @@ ffmpeg.setFfmpegPath(ffmpegStatic)
 module.exports = class FFmpeg {
 	static encode({ input, inputOptions, videoCodec, complexFilter, audioCodec, noAudio = false, outputOptions, outputFilePath } = {}) {
 		return new Promise((resolve, reject) => {
-			const ff = ffmpeg()
+      try {
+        const ff = ffmpeg()
 			
-			if (!Array.isArray(input)) input = [input]
+			  if (!Array.isArray(input)) input = [input]
 	
-			for (let i = 0; i < input.length; i++) {
-				ff.input(input[i])
-			}
+			  for (let i = 0; i < input.length; i++) {
+				  ff.input(input[i])
+			  }
 	
-			if (inputOptions && typeof inputOptions === 'object') {
-				for (const option in inputOptions) {
-					ff.inputOptions(option, inputOptions[option])
-				}
-			}
+			  if (inputOptions && typeof inputOptions === 'object') {
+				  for (const option in inputOptions) {
+					  ff.inputOptions(option, inputOptions[option])
+				  }
+			  }
 	
-			if (videoCodec) ff.videoCodec(videoCodec)
+			  if (videoCodec) ff.videoCodec(videoCodec)
 	
-			if (complexFilter) ff.complexFilter(complexFilter)
+			  if (complexFilter) ff.complexFilter(complexFilter)
 	
-			if (noAudio) {
-				ff.noAudio()
-			} else if (audioCodec) {
-				ff.audioCodec(audioCodec)
-			}
+			  if (noAudio) {
+				  ff.noAudio()
+			  } else if (audioCodec) {
+				  ff.audioCodec(audioCodec)
+			  }
 			
-			if (outputOptions && typeof outputOptions === 'object') {
-				for (const option in outputOptions) {
-					ff.outputOptions(option, outputOptions[option])
-				}
-			}
+			  if (outputOptions && typeof outputOptions === 'object') {
+				  for (const option in outputOptions) {
+					  ff.outputOptions(option, outputOptions[option])
+				  }
+			  }
 	
-			ff.saveToFile(outputFilePath)
+			  ff.saveToFile(outputFilePath)
 	
-			if (!isProduction) {
-				ff.on('progress', (progress) => {
-					if (progress.percent) {
-						console.log(`Processing: ${Math.floor(progress.percent)}% done`)
-					}
-				})
-			}
+			  if (!isProduction) {
+				  ff.on('progress', (progress) => {
+					  if (progress.percent) {
+						  console.log(`Processing: ${Math.floor(progress.percent)}% done`)
+					  }
+				  })
+			  }
 	
-			ff.on('end', resolve)
-			ff.on('error', reject)
+			  ff.on('end', resolve)
+			  ff.on('error', reject)
+      } catch (ex) {
+        reject(ex)
+      }
 		})
 	}
 
