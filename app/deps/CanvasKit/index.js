f346cdcb9a0d8c48f3743df154b81c960dbee3ab
Anton Piliugin <anton.piliugin@icloud.com>
2023-10-14 10:05:12 +0500

    CanvasKit

--- app/deps/CanvasKit/index.js
+++ app/deps/CanvasKit/index.js
@@ -3,16 +3,11 @@ const path = require('path')
 const fs = require('fs')
 
 module.exports = class CanvasKit {
-  instance = null
-
   static async getCanvas(width, height) {
-    if (!CanvasKit.instance) {
-      CanvasKit.instance = await CanvasKitInit({
-        locateFile: (file) => path.join(__dirname, '..', '..', '..', 'node_modules', 'canvaskit-wasm', 'bin', file)
-      })
-    }
-
-    const canvas = CanvasKit.instance.MakeCanvas(width, height)
+    const instance = await CanvasKitInit({
+      locateFile: (file) => path.join(__dirname, '..', '..', '..', 'node_modules', 'canvaskit-wasm', 'bin', file)
+    })
+    const canvas = instance.MakeCanvas(width, height)
 
     canvas.encodeImage = encodeImage
     canvas.saveImage = saveImage
